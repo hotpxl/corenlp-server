740a7798214e7cf0fbb9403b4b3ebe44d6ea7680
Yutian Li <hotpxless@gmail.com>
2016-07-17 18:24:33 +0800

    [master] Push to current path.

--- get-corenlp.sh
+++ get-corenlp.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -euo pipefail
+
+file_path="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+pushd "${file_path}" > /dev/null
+
 curl --fail --location --remote-header-name --remote-name "http://nlp.stanford.edu/software/stanford-corenlp-full-2015-12-09.zip"
 unzip "./stanford-corenlp-full-2015-12-09.zip"
